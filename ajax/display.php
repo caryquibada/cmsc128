6f3d49362cc6ca484af28f36b7f5b8715d03697b
caryquibada <cmquibada@up.edu.ph>
2018-02-05 16:41:16 +0800

    Added printing of currently timed in students

---
+++ ajax/display.php
@@ -0,0 +1,33 @@
+<?php
+$connect = mysqli_connect('localhost','root','');
+
+if(!$connect){
+    echo 'No connection to server';
+}
+if(!mysqli_select_db($connect,'upblibusage')){
+    echo 'Database "lukedb" is not selected';
+}
+$sql="SELECT * FROM transaction";
+$result=mysqli_query($connect,$sql);
+
+echo "  
+        <thead>
+        <tr>
+        <th>Transaction Number</th>
+        <th>Student Number</th>
+        <th>Time-in</th>
+        <th>Time-out</th>
+        </tr>
+        </thead>
+        <tbody>";
+
+while($row=mysqli_fetch_row($result)){
+    echo "<tr>
+          <td>$row[0]</td>
+          <td>$row[1]</td>
+          <td>$row[2]</td>
+          <td>Timeout</td>
+          </tr>";
+}
+echo "</tbody>";
+?>
--- index.php
+++ index.php
@@ -1,8 +1,11 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/ju/jq-3.2.1/dt-1.10.16/r-2.2.1/rr-1.2.3/datatables.min.css"/>
-    <script type="text/javascript" src="https://cdn.datatables.net/v/ju/jq-3.2.1/dt-1.10.16/r-2.2.1/rr-1.2.3/datatables.min.js"></script>
-    
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+   <link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.0/css/jquery.dataTables_themeroller.css">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
     </head>
     <body>
         <form action="ajax/input.php" method="post" class="ajax">
@@ -12,15 +15,33 @@
         </input>
         </br>
         </form>
+        <div class="col-lg-6"><table class="table table-striped table-hover table-sm" id="tableHolder"></table></div>
+       
        
     </body>
 </html>
-<script  src="http://code.jquery.com/jquery-3.3.1.js"
-			integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-		    crossorigin="anonymous"></script>
-    <script src="js/input.js"></script>
-<script>
-$(document).ready(function(){
-    $('#myTable').DataTable();
-});
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+      refreshTable();
+     
+    });
+
+    function refreshTable(){
+        $('#tableHolder').load('ajax/display.php', function(){
+           setTimeout(refreshTable, 3000);
+        });
+        
+    }
 </script>
+
+
+    <script src="js/input.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function(){
+        $('#tableHolder').DataTable();
+    });
+    </script>
