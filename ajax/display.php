70d5ee0a74cdf297957f25f33fa6a0d7ed71a926
caryquibada <cmquibada@up.edu.ph>
2018-02-17 07:26:43 +0800

    changed index table to be datatables

--- ajax/display.php
+++ ajax/display.php
@@ -4,7 +4,17 @@ include 'conn.php';
 $sql="SELECT * FROM transaction WHERE YEAR(time_out)='0000'";
 $result=mysqli_query($connect,$sql);
 //ID #2
-
+echo "<thead>
+<tr>
+    <th>Tag Number</th>
+    <th>Transaction Number</th>
+    <th>Student Number</th>
+    <th>Time-in</th>
+    <th>Time-out</th>
+    
+</tr>
+</thead>
+<tbody id="."'tableBody'".">";
 
 while($row=mysqli_fetch_row($result)){
     echo "<tr>
@@ -16,4 +26,5 @@ while($row=mysqli_fetch_row($result)){
             <td><button class="."'btnSelect'"." id=".$row[0]." name="."'test'"." onclick="."'Alert1();load();load1();'".">Time-out</button></td>
           </tr>";
 }
+echo "</tbody>";
 ?>
