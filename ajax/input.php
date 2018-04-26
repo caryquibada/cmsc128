c139ad50865eaf884e62a27fd07abfd3c1cf2a6d
caryquibada <cmquibada@up.edu.ph>
2018-04-26 11:54:25 +0800

    added checker if student is in database and added more error handling in inputs

--- ajax/input.php
+++ ajax/input.php
@@ -1,12 +1,10 @@
 <?php
 include 'conn.php';
 //#1
-echo "test";
 if (isset($_POST['studentNumber'])&&!empty($_POST['studentNumber'])){
     $sn=$_POST['studentNumber'];
     $tagno=$_POST['tagnumber'];
     $type=$_POST['type'];
-    echo $tagno;
 
     $query="SELECT * FROM student";
     $checker=0;
@@ -15,15 +13,12 @@ if (isset($_POST['studentNumber'])&&!empty($_POST['studentNumber'])){
         $output=$row['student_number'];
         if($output==$sn){
             $checker=1;
+            break;
         }
     }
-
+    echo $checker;
     if($checker==0){
-        $studentquery="INSERT INTO student(student_number) VALUES ('$sn')";
-        if(!mysqli_query($connect,$studentquery)){
-            echo $sn;
-            echo "Error1";
-        }
+        $response_array['status'] = 'error';  
     }else{
         $sql="INSERT INTO transaction(student_number,tag_no,type) VALUES ('$sn','$tagno','$type')";
         if(!mysqli_query($connect,$sql)){
@@ -31,6 +26,7 @@ if (isset($_POST['studentNumber'])&&!empty($_POST['studentNumber'])){
             echo "Error";
         }
     }
+    
 }
 //End of #1
 ?>
