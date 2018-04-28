475f17bb1efb17c63843bd94ca9bbd89b975acda
caryquibada <cmquibada@up.edu.ph>
2018-04-28 12:27:44 +0800

    Revert "added more error handling"
    This reverts commit 23f183d388b1c7e5bc5119849a87b562810ef932.

--- ajax/input.php
+++ ajax/input.php
@@ -16,42 +16,16 @@ if (isset($_POST['studentNumber'])&&!empty($_POST['studentNumber'])){
             break;
         }
     }
-    $query="SELECT student_number from transaction WHERE time_out='0000-00-00 00:00:00'";
-    while($row=mysqli_fetch_array(mysqli_query($connect,$query))){
-        if($row[0]==$sn){
-            $checker=4;
-            echo $checker;
-            die();
-        }
-    }
+    echo $checker;
     if($checker==0){
         $response_array['status'] = 'error';  
     }else{
-        $remQuery="SELECT time_remaining from student WHERE student_number='$sn'";
-        $result=mysqli_fetch_row(mysqli_query($connect,$remQuery));
-        if(((int)$result[0]<=0)&&$type=='Power_Usage'){
-            $checker=2;
-        }else{
-            if($type=="Computer_Usage"){
-                $checker=3;
-                $sql="INSERT INTO transaction(student_number,tag_no,type) VALUES ('$sn','$tagno','$type')";
-                if(!mysqli_query($connect,$sql)){
-                    echo $sql;
-                    echo "Error";
-                }
-            }else{
-                $sql="INSERT INTO transaction(student_number,tag_no,type) VALUES ('$sn','$tagno','$type')";
-                    if(!mysqli_query($connect,$sql)){
-                        echo $sql;
-                        echo "Error";
-                    }
-            }
-            
+        $sql="INSERT INTO transaction(student_number,tag_no,type) VALUES ('$sn','$tagno','$type')";
+        if(!mysqli_query($connect,$sql)){
+            echo $sql;
+            echo "Error";
         }
-        
     }
-    
-    echo $checker;
     
 }
 //End of #1
